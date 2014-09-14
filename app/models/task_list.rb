e703f301ce047f0ad1127732b67c23692d1ca94b
Joe Grady <jogrady1@gmail.com>
2014-09-13 22:37:19 -0400

    added jpg to readme

--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -2,7 +2,7 @@
 # Allows for validation
 class Task_List < ActiveRecord::Base
   has_many :users_tasks
-  has_many :tasks through: users_tasks:, dependent: :destroy
+  has_many :tasks, through: :users_tasks, dependent: :destroy
   has_many :tasks, dependent: :destroy
 
   validates :name, presence: true
--- app/models/user_task.rb
+++ app/models/user_task.rb
@@ -5,6 +5,5 @@ class Users_Task < ActiveRecord::Base
   belongs_to :tasks_lists
   has_many :tasks
 
-  validates :task_list_id, presence: true
   validates :user_id, presence: true
 end
