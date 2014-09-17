bba1aa505531e6c4efd2ee6d65f06cb731709bb1
Helen Hood <helen.c.hood@gmail.com>
2014-09-17 10:36:15 -0400

    Remove unnecessary has_many through association

--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -2,7 +2,13 @@
 # Allows for validation
 class Task_List < ActiveRecord::Base
   has_many :users_tasks
-  has_many :tasks, through: :users_tasks, dependent: :destroy
+
+  # You don't actually want a task_id on your join table, so you
+  # shouldn't have a has_many through association for tasks.
+  # You can grab all the tasks associated with a
+  # task_list with a simple has_many :tasks, since the tasks table
+  # has a task_list_id field:
+  # has_many :tasks, through: :users_tasks, dependent: :destroy
   has_many :tasks, dependent: :destroy
 
   validates :name, presence: true
